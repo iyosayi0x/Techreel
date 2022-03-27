698b97fbcbab1f7480edc668f6a22220281e041c
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2022-03-27 22:06:06 +0100

    reduced featured posts to 3 and ui updated

--- techreel-backend/accounts/forms.py
+++ techreel-backend/accounts/forms.py
@@ -7,12 +7,13 @@ Account = get_user_model()
 
 
 class AccountCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label="password" , widget=forms.PasswordInput)
-    password2 = forms.CharField(label='re_password' , widget=forms.PasswordInput)
+    password1 = forms.CharField(label="password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label='re_password', widget=forms.PasswordInput)
 
     class Meta:
         model = Account
-        fields = ('username','email',)
+        fields = ('username', 'email',)
 
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
@@ -22,7 +23,7 @@ class AccountCreationForm(forms.ModelForm):
             raise forms.ValidationError("Passwords do not match")
         return password2
 
-    def save(self , commit=True):
+    def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password1'])
         if commit:
@@ -35,4 +36,5 @@ class AccountChangeForm(forms.ModelForm):
 
     class Meta:
         model = Account
-        fields = ('email', 'username' , 'password' , 'superuser' , 'staff' , 'is_active',)
+        fields = ('email', 'username', 'password',
+                  'superuser', 'staff', 'is_active',)
--- techreel-backend/blog/views.py
+++ techreel-backend/blog/views.py
@@ -9,17 +9,24 @@ from django.utils import timezone
 from .utils import BlogPostPagination
 
 # works
+
+
 class BlogPostFeaturedView(ListAPIView):
     queryset = BlogPost.objects.all().filter(featured=True)
     serializer_class = BlogPostSerializer
 
 # works
+
+
 class BlogPostListView(ListAPIView):
     pagination_class = BlogPostPagination
-    queryset = BlogPost.objects.all().filter(featured=False).order_by('-date_created')
+    queryset = BlogPost.objects.all().filter(
+        featured=False).order_by('-date_created')
     serializer_class = BlogPostSerializer
 
 # works
+
+
 class BlogPostSearchView(APIView):
     def post(self, request):
         data = request.data
@@ -31,32 +38,39 @@ class BlogPostSearchView(APIView):
             lookup |= Q(title__icontains=search_item)
         # use lookup to filter post
         queryset = BlogPost.objects.all().filter(lookup)
-        serializer = BlogPostSerializer(queryset , many=True)
-        return Response(serializer.data , status=status.HTTP_200_OK)
+        serializer = BlogPostSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 # works
+
+
 class BlogPostTagFilterView(APIView):
     def post(self, request):
         data = request.data
         tag = data.get('tag')
         queryset = BlogPost.objects.filter(tags__icontains=tag)
-        serializer = BlogPostSerializer(queryset , many=True)
-        return Response(serializer.data , status=status.HTTP_200_OK)
+        serializer = BlogPostSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 # works
+
+
 class BlogPostListPopularView(ListAPIView):
     # we only want the post within the last 7 days
-    queryset = BlogPost.objects.all().filter(date_created__range=[timezone.now() - timezone.timedelta(7) , timezone.now()]).order_by('-views')[:4]
+    queryset = BlogPost.objects.all().filter(date_created__range=[
+        timezone.now() - timezone.timedelta(7), timezone.now()]).order_by('-views')[:3]
     serializer_class = BlogPostSerializer
 
 # works
+
+
 class BlogPostDetailView(APIView):
-    def get(self, request , slug):
+    def get(self, request, slug):
         try:
             queryset = BlogPost.objects.get(slug__iexact=slug)
             queryset.views += 1
             queryset.save()
             serializer = BlogPostSerializer(queryset)
-            return Response(serializer.data , status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except BlogPost.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
