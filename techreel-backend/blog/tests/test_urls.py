ea115a745e5c0d2087f94149d3180455c92428b6
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2022-03-30 01:34:51 +0100

    Added test for blog post urls

---
+++ techreel-backend/blog/tests/test_urls.py
@@ -0,0 +1,36 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from blog.views import (BlogPostListView,
+                        BlogPostFeaturedView,
+                        BlogPostSearchView,
+                        BlogPostTagFilterView,
+                        BlogPostListPopularView,
+                        BlogPostDetailView
+                        )
+
+
+class TestUrls(SimpleTestCase):
+    def test_blog_post_list_resolves(self):
+        url = reverse('blogpost_list')
+        self.assertEquals(resolve(url).func.view_class, BlogPostListView)
+
+    def test_blog_post_featured_resolves(self):
+        url = reverse('blogpost_featured')
+        self.assertEquals(resolve(url).func.view_class, BlogPostFeaturedView)
+
+    def test_blog_post_search_resolves(self):
+        url = reverse('blogpost_search')
+        self.assertEquals(resolve(url).func.view_class, BlogPostSearchView)
+
+    def test_blog_post_featured_resolves(self):
+        url = reverse('blogpost_filter')
+        self.assertEquals(resolve(url).func.view_class, BlogPostTagFilterView)
+
+    def test_blog_post_popular_resolves(self):
+        url = reverse('blogpost_popular')
+        self.assertEquals(resolve(url).func.view_class,
+                          BlogPostListPopularView)
+
+    def test_blog_post_detail_resolves(self):
+        url = reverse('blogpost_detail', args=['slug-param'])
+        self.assertEquals(resolve(url).func.view_class, BlogPostDetailView)
