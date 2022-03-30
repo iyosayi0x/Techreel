1796f97c5f2e030c1214e837d42410b50b827057
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2022-03-30 01:36:13 +0100

    Updated component

--- techreel-frontend/src/views/Sitemap.jsx
+++ techreel-frontend/src/views/Sitemap.jsx
@@ -1,7 +1,10 @@
 const Sitemap=()=>{
     return (
         <div className='sitemap'>
+            <h1 className='sitemap_header'>Sitemap</h1>
+            <section className='sitemap_links_wrapper'>
 
+            </section>
         </div>
     )
 }
