4b67b3d309adae74cb1f54a5e3682dd8bd1b913f
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2022-03-30 01:35:30 +0100

    Added name param to blog post urls

--- techreel-backend/blog/urls.py
+++ techreel-backend/blog/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import BlogPostDetailView , BlogPostListView , BlogPostFeaturedView , BlogPostSearchView , BlogPostListPopularView , BlogPostTagFilterView
+from .views import BlogPostDetailView, BlogPostListView, BlogPostFeaturedView, BlogPostSearchView, BlogPostListPopularView, BlogPostTagFilterView
 
 urlpatterns = [
-    path('', BlogPostListView.as_view()),
-    path('featured/', BlogPostFeaturedView.as_view()),
-    path('search/', BlogPostSearchView.as_view()),
-    path('popular/' , BlogPostListPopularView.as_view()),
-    path('tag/' , BlogPostTagFilterView.as_view()),
-    path('<str:slug>/', BlogPostDetailView.as_view()),
+    path('', BlogPostListView.as_view(), name='blogpost_list'),
+    path('featured/', BlogPostFeaturedView.as_view(), name='blogpost_featured'),
+    path('search/', BlogPostSearchView.as_view(),  name='blogpost_search'),
+    path('popular/', BlogPostListPopularView.as_view(), name='blogpost_popular'),
+    path('tag/', BlogPostTagFilterView.as_view(), name='blogpost_filter'),
+    path('<str:slug>/', BlogPostDetailView.as_view(), name='blogpost_detail'),
 ]
