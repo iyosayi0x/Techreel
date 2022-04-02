dc7ca637f02acdd67121367c6778a90c3d8069ae
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2022-04-02 14:27:36 +0100

    Formatted date and implemented serializer relation on author field

--- techreel-backend/blog/serializers.py
+++ techreel-backend/blog/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import BlogPost
 
+
 class BlogPostSerializer(serializers.ModelSerializer):
+    author = serializers.CharField(max_length=200)
+    date_created = serializers.DateTimeField(format='%b %d %Y')
+
     class Meta:
         model = BlogPost
-        fields = ('author','id', 'title', 'slug' , 'tags', 'thumbnail','exert','content', 'featured' , 'date_created',)
+        fields = ('author', 'id', 'title', 'slug', 'tags', 'thumbnail',
+                  'exert', 'content', 'featured', 'date_created',)
