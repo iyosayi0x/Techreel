b32f145a2e40a53dc33e45919cfd660e1cf9a89d
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2022-04-04 23:35:30 +0100

    Created serializer for author model

---
+++ techreel-backend/authors/serializers.py
@@ -0,0 +1,7 @@
+from .models import Author 
+from rest_framework import serializers 
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author 
+        fields = ('name','twitter_handle',)
