99913c5df53f14a6ac59636dc51093b34eb6bb45
iyosayi-dexter <onaiwuiyosayi@gmail.com>
2022-04-04 23:34:23 +0100

    Added new field to author model and made migrations

--- techreel-backend/authors/models.py
+++ techreel-backend/authors/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=50)
+    twitter_handle = models.CharField(max_length=50 , default='@iyosayi18')
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
